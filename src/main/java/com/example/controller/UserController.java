82005d955706406cc0bd8116528d26e10da81b5f
mura <mura@example.com>
2026-05-11 12:03:16 +0900

    "feat: HttpSessionを使ったカート機能を追加"

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.ecsample.form.UserForm;
+import com.example.form.UserForm;
 
 @Controller
 public class UserController {
--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,4 +1,4 @@
-package com.example.ecsample.form;
+package com.example.form;
 
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
